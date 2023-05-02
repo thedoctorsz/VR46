fc1c47c06260e30f12a23733f8cc03925da89069
VR46 <dedoctorsz@gmail.com>
2023-05-02 21:22:51 +0000

    VR46: Build ``BananaDroid`` Thirteen * Build with gapps
    Change-Id: I5be36d914c9297353a5898dbcdb7e5074b8279b2
    Signed-off-by: VR46 <dedoctorsz@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b superior-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b banana-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME="$USERNAME"
 export BUILD_HOSTNAME="$HOSTNAME"
-lunch superior_whyred-userdebug
+lunch banana_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-m bacon -j8 > reading #& sleep 95m # Jangan di hapus text line (> reading)
+m banana -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
