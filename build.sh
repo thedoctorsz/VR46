5e0fe115b1e57b700b1bd5a01178bd20a9493f18
VR46 <dedoctorsz@gmail.com>
2023-05-05 22:42:54 +0000

    VR46: lavender: build: RisingOS Babylon
    Change-Id: I76c7bc8054400a4c9d7820c7923160de09c0b92e
    Signed-off-by: VR46 <dedoctorsz@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b rising-13 .repo/local_manifests
+git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b lavender-rising-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME="$USERNAME"
 export BUILD_HOSTNAME="$HOSTNAME"
-lunch lineage_whyred-userdebug
+lunch lineage_lavender-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka bacon -j8 > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka bacon -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
